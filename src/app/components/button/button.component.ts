df24d9d650e49cd60b1411caba37c7bdd2b59247
David Toledo <dtoledoz0207@gmail.com>
2021-05-23 12:43:14 -0500

    creates-header-and-button-components

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() color?: string;
+  @Input() text?:string;
+
+  @Output() onClickEvent = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(): void {
+    this.onClickEvent.emit();
+  }
+
+}
