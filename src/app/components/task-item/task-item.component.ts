b6a34ba8234702c96954aebaeef68fd4b00ac3ff
David Toledo <dtoledoz0207@gmail.com>
2021-05-23 13:27:00 -0500

    creates-tasks-and-taskItem-components-also-adds-mock-tasks-file-and-TaskInterface-finally-adds-fontAwesome-for-Angular

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Task } from '../../interfaces/Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  @Input() task:Task = <Task>{};
+
+  faTimes = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
