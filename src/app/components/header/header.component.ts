e0adf962a18f7984e3ed57130b4cdfb5b600a9ad
David Toledo <dtoledoz0207@gmail.com>
2021-05-24 10:43:45 -0500

    creates-aboutComponent-and-footerComponent-also-implements-routes-and-hidde-addButton-in-aboutRoute

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 import { UiService } from '../../services/ui.service';
 
 @Component({
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = false;
   subscription: Subscription;
 
-  constructor(private uiService:UiService) {
+  constructor(private uiService:UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
   }
 
@@ -22,6 +23,10 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask() {
     this.uiService.toggleAddTask()
+  }
+
+  hasRoute(route: string): boolean {
+    return this.router.url === route;
   }
 
 }
